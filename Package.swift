86fc438d388a608776966a342d957d168bf3948a
BI1EQZ <77913158+BI1EQZSupery@users.noreply.github.com>
2024-07-28 10:13:13 +0800

    Update Package.swift
    rm FL Prefix

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "FLMosquitto",
+    name: "Mosquitto",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
